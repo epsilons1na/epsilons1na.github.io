a9ac4b0e6359ee3e0dc85a6ea88688297a5491fc
epsilon <prasadjanardan735@gmail.com>
2026-05-22 12:35:09 +0530

    changed bio

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/stare.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Epsilon",
-	bio: "Autistic pwner",
+	bio: "Artistic pwner",
 	links: [
 		{
 			name: "Twitter",
